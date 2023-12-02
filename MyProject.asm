1a442b67686fd6b0152453a565ab9ea705506d8a
Dore8 <151974469+Dore8@users.noreply.github.com>
2023-12-01 17:17:25 -0800

    Update MyProject.asm

--- MyProject.asm
+++ MyProject.asm
@@ -7,12 +7,14 @@
 .end_macro
 
 .macro printInts
+	# if element in array is equal to 10 or 11, it jumos to print0 or printX respectively
 	beq $t0, 10, print0
 	beq $t0, 11, printX
 	
+	# print out array element
 	li $v0, 1
 	move $a0, $t0
-	syscall
+	syscall 
 	
 	
 	
@@ -37,7 +39,7 @@ array: .word 1, 2, 3, 4, 5, 6, 7, 8, 9
 
 O: .asciiz "O"
 X: .asciiz "X"
-allInputs: .word 0, 0, 0, 0, 0, 0, 0, 0, 0
+#allInputs: .word 0, 0, 0, 0, 0, 0, 0, 0, 0
 Oinputs: .word 0, 0, 0, 0, 0
 Xinputs: .word 0, 0, 0, 0, 0
 
@@ -47,21 +49,17 @@ player2Move: .asciiz "\nPlayer 2, please enter the slot you would like to take: 
 .text
 main:	
 	
-	move $s3, $sp
-	addi $sp, $sp, 76
-	la $a0, 20($sp) # Oinput
-	#move $s5, 40($sp) # 
-	#move $s6, 76($sp)
-	
+	# print out rules
 	printStr(rules1)
 	printStr(rules2)
 	printStr(rules3)
 	printStr(rules4)
+	
 	j askForMove1
 	
 	
 
-printTable:
+printTable: # prints out the table 
 	
 	
 	#load current element into $t0
@@ -69,28 +67,21 @@ printTable:
 	
 	#increment base address by 4
 	addi $s0, $s0, 4
+	
 	#increment counter by 1
 	addi $t1, $t1, 1
 	
-	#new
-	
-	
-	
-	#new
-	#beq $t1, 10, print0
-	
+	# print out integer element/check if X or O in that space
 	printInts
 	
-	
-	
-	
-	#implement conditional to break out of the loop
-	#once the counter reaches 5, we should exit
+	#if counter equals 3 call printEnd
 	beq $t1, 3, printEnd
+	#if counter equals 6 call printEnd
 	beq $t1, 6, printEnd
+	#if counter equals 9 call checkTurn
 	beq $t1, 9, checkTurn
 	
-	
+	# print out spacer, " | "
 	printStr(spacer)
 	
 	
@@ -98,183 +89,186 @@ printTable:
 	
 	j printTable
 	
-prepareArray:
+prepareArray: # restart counter and prepares array to be used in printTable
+
+	# load address of array in $s0
 	la $s0, array
 	printStr(endspacer)
-	#loop counter
+	
+	# set loop counter to zero
 	move $t1, $zero
+	
 	j printTable
 	
-askForMove1:
-
-	#///FIX/// jumping to printBeg doesn't work? initial board build?
-	#begin game
-	#jal printBeg #assuming this prints only board with only numbers
-	#//trying to print board with what has already been played
-
-	li $v0, 4		#ask player 1 for their move
+askForMove1: # prompts user, stores user input, and indicates that its player2s turn next
+	
+	#ask player 1 for their move
+	li $v0, 4		
 	la $a0, player1Move
 	syscall
 	
-	li $v0, 5		#save integer imput to $s0
+	#save integer input to $s4
+	li $v0, 5		
 	syscall
-	#move $s7, $v0
 	move $s4, $v0
 	
-    	
+    	# load 12 into $t2 and move it to $s6
+    	# Indicates that Player1's turn is complete and that Player2 is next (see checkTurn )
 	li $t2, 12
 	move $s6, $t2
 	
 	jal XchangeArray
 	
-	#j prepareArray
-	
-	#branch if less than 1 or less than 9 to re-prompt for correct user input from Player 1
-	#blt $s0, 1, invalidSlotPrompt1
-	#bgt $s0, 9, invalidSlotPrompt1
 
-	#jal printBeg
-	#//branch
-	#branch to replace number with 'X', then branch back to askForMove2
-	
 askForMove2:
 
-	#//print board with all previous moves
 	
-	li $v0, 4		#ask player 2 for their move
+	#ask player 2 for their move
+	li $v0, 4		
 	la $a0, player2Move
 	syscall
 	
-	#li $v0, 5		#save integer input to $s0
-	#syscall
-	#move $s7, $v0
-	
+	#save integer input to $s4
 	li $v0, 5
 	syscall
 	move $s4, $v0
-	
-	#jal OchangeArray
-	
-    	#sw $v0, Oinputs($s1)
-	#increment base addresss by 4
     	
-	
+	# load 11 into $t2 and move it to $s6
+    	# Indicates that Player2's turn is complete and that Player1 is next (see checkTurn )
 	li $t2, 11
 	move $s6, $t2
 	
 	jal OchangeArray
 	
-	#j prepareArray
 
-	#branch if less than 1 or less than 9 to re-prompt for correct user input from Player 2
-	#blt $s0, 1, invalidSlotPrompt2
-	#bgt $s0, 9, invalidSlotPrompt2
+OchangeArray: # change the value in the array to a 10, which symbolizes a 'O'
 
-	#//branch
-	#branch to replace number with 'O', then branch back to askForMove1
-
-OchangeArray:
-#new
+	# load address of array in $s0
 	la $s0, array
 	
 	#load current element into $t0
 	lw $t0, ($s0)
-	la $a2,($s0)
+	
+	#la $a2,($s0)
+	
 	subi $s4, $s4, 1
+	
+	# multiply $s4(user input) by 4 (bytes) to get the location of the wanted element in array and store in $s5
 	mul $s5, $s4, 4
+	# add $s5 to array address to get the address of the element we want to change
 	add $s0, $s0, $s5
 	
+	# load the current element into $t3
+	lw $t3, ($s0)
+	
+	# check if $t3, equals 10 and 11 jump to 'askForMove2'
+	# the 10 and 11 means that there already is a O or X in that space
+	beq $t3, 11, askForMove2
+	beq $t3, 10, askForMove2
+	
+	# load 10,(O), into $t5
+	# then store in array
 	li $t5, 10
-	
 	sw $t5, ($s0)
-	#incremeent base adress
-	#addi $s1, $s1, 4
+	
 	
 	j prepareArray
 
 
-#new
 
-XchangeArray:
-#new
+XchangeArray: # change the value in the array to a 11, which symbolizes a 'X'
+
+	# load address of array in $s0
 	la $s0, array
 	
 	#load current element into $t0
 	lw $t0, ($s0)
-	la $a2,($s0)
+	
+	#la $a2,($s0)
+	
 	subi $s4, $s4, 1
+	
+	# multiply $s4(user input) by 4 (bytes) to get the location of the wanted element in array and store in $s5
 	mul $s5, $s4, 4
+	# add $s5 to array address to get the address of the element we want to change
 	add $s0, $s0, $s5
 	
-	li $t5, 11
 	
+	# load the current element into $t3
+	lw $t3, ($s0)
+	
+	# check if $t3, equals 10 and 11 jump to 'askForMove2'
+	# the 10 and 11 means that there already is a O or X in that space
+	beq $t3, 11, askForMove1
+	beq $t3, 10, askForMove1
+	
+
+	# load 11,(X), into $t5
+	# then store in array
+	li $t5, 11
 	sw $t5, ($s0)
-	#incremeent base adress
-	#addi $s1, $s1, 4
+	
 	
 	j prepareArray
 
 
-#new
 
 
-checkTurn:
-
+checkTurn: # check which players turn it is
+	
+	# if $t2, equals 11, jump to askForMove1
 	beq $t2, 11, askForMove1
+	# if $t2, equals 12, jump to askForMove2
 	beq $t2, 12, askForMove2
 
 print0:
 	
-	
+	# print out O
 	li $v0, 4
 	la $a0, O
 	syscall
 	
-	#increment base address by 4
-	#addi $s0, $s0, 4
-	#increment counter by 1
-	#addi $t1, $t1, 1
-	
+	# if the $t1(current array element) is 9, we jump to checkTurn
 	beq $t1, 9,checkTurn
 	
-	
+	# if the $t1(current array element) is 3, we jump to printEnd
 	beq $t1, 3, printEnd
+	# if the $t1(current array element) is 6, we jump to printEnd
 	beq $t1, 6, printEnd
 	
+	# print out space, " | "
 	printStr(spacer)
 	
 	j printTable
 	
-#new
+
 printX:
 	
-	
+	# print out X
 	li $v0, 4
 	la $a0, X
 	syscall
 	
-	#increment base address by 4
-	#addi $s0, $s0, 4
-	#increment counter by 1
-	#addi $t1, $t1, 1
-	
+	# if the $t1(current array element) is 9, we jump to checkTurn
 	beq $t1, 9,checkTurn
 	
-	
+	# if the $t1(current array element) is 3, we jump to printEnd
 	beq $t1, 3, printEnd
+	# if the $t1(current array element) is 6, we jump to printEnd
 	beq $t1, 6, printEnd
 	
+	# print out space, " | "
 	printStr(spacer)
 	
 	j printTable
-#new
 
-printBeg:
+
+printBeg: # print at the beginning of line when printing table
 	printStr(newline)
 	printStr(endspacer)
 	j printTable
 	
-printEnd:
+printEnd: # print at the end of line when printing table
 	printStr(endspacer)
 	printStr(line)
 	printStr(newline)
@@ -284,7 +278,7 @@ printEnd:
 
 	
 	
-exit:
+exit: # exit program
 	li $v0, 10
 	syscall
 	
